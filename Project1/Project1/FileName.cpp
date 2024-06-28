5ac00885dd0fac2a76663cbdcd2ebde19b90f8cc
Slipchuk Max <maxslipchuk11@gmail.com>
2024-06-28 17:37:20 +0300

    Add Zmini and Enter Radki

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -7,5 +7,14 @@
 using namespace std;
 
 int main() {
+    srand(time(NULL));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char str[50];
+    char str1[10];
 
+    cout << "¬вед≥ть р€док: ";
+    cin.getline(str, 50);
+    cout << "¬вед≥ть символи €к≥ хочете видаленн€: ";
+    cin.getline(str1, 10);
 }
