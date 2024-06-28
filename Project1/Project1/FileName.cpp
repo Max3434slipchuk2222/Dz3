c43b0ecfcdbcd9710ff550e32e849c45d8c39fd4
Slipchuk Max <maxslipchuk11@gmail.com>
2024-06-28 17:39:29 +0300

    Add removesymbols function

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -5,6 +5,26 @@
 #include <ctime>
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
+void removeSymbols(char* str, const char* str1) {
+    int a = 0;
+    int b = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        b = 0;
+        for (int j = 0; str1[j] != '\0'; ++j) {
+            if (str[i] == str1[j]) {
+                b = 1;
+                break;
+
+            }
+        }
+        if (b == 0) {
+            str[a] = str[i];
+            a++;
+        }
+    }
+    str[a] = '\0';
+}
 
 int main() {
     srand(time(NULL));
